458036cd3ee588067a9ddb2afc1001066fe66f74
pyIDM <mahmoud.aboghazala@gmail.com>
2020-03-23 22:17:46 +0200

    fixed requirements.txt not found error, issue #60 when building from  source code on PyPi, there is no requirements.txt

--- setup.py
+++ setup.py
@@ -1,30 +1,39 @@
 """
-    pyIDM
+    PyIDM
 
     multi-connections internet download manager, based on "pyCuRL/curl", "youtube_dl", and "PySimpleGUI"
 
     :copyright: (c) 2019-2020 by Mahmoud Elshahat.
     :license: GNU LGPLv3, see LICENSE for more details.
 """
-import setuptools, os, sys
+
+import os
+import setuptools
+
+# get current directory
+path = os.path.realpath(os.path.abspath(__file__))
+current_directory = os.path.dirname(path)
 
 # get version
 version = {}
-with open("pyidm/version.py") as f:
-    exec(f.read(), version) # later on we use: version['__version__']
+with open(f"{current_directory}/pyidm/version.py") as f:
+    exec(f.read(), version)  # then we can use it as: version['__version__']
 
 # get long description from readme
-with open("README.md", "r") as fh:
+with open(f"{current_directory}/README.md", "r") as fh:
     long_description = fh.read()
 
 # get requirements
-with open("requirements.txt", "r") as fh:
-	requirements = fh.readlines()
+try:
+    with open(f"{current_directory}/requirements.txt", "r") as fh:
+        requirements = fh.readlines()
+except:
+    requirements = ['PySimpleGUI', 'pyperclip', 'plyer', 'certifi', 'youtube_dl', 'pycurl', 'pillow']
 
 setuptools.setup(
     name="pyIDM",
     version=version['__version__'],
-    scripts=[], # ['pyIDM.py'], no need since added an entry_points
+    scripts=[],  # ['pyIDM.py'], no need since added an entry_points
     author="Mahmoud Elshahat",
     author_email="mahmoud_elshahhat@yahoo.com",
     description="download manager",
@@ -32,23 +41,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pyIDM/pyIDM ",
     packages=setuptools.find_packages(),
-    keywords="internet download manager youtube downloader pycurl youtube-dl PySimpleGUI",
+    keywords="internet download manager youtube downloader pycurl curl youtube-dl PySimpleGUI",
     project_urls={
-    'Source': 'https://github.com/pyIDM/pyIDM',
-    'Tracker': 'https://github.com/pyIDM/pyIDM/issues',
-    'Releases': 'https://github.com/pyIDM/pyIDM/releases',
-    'Screenshots': 'https://github.com/pyIDM/pyIDM/issues/13'
+        'Source': 'https://github.com/pyIDM/pyIDM',
+        'Tracker': 'https://github.com/pyIDM/pyIDM/issues',
+        'Releases': 'https://github.com/pyIDM/pyIDM/releases',
+        'Screenshots': 'https://github.com/pyIDM/pyIDM/issues/13#issuecomment-602136747'
     },
-    install_requires= requirements, #['PySimpleGUI', 'pyperclip', 'plyer', 'certifi', 'youtube_dl', 'pycurl'], 
-    # py_modules=[],
-    # data_files=[(os.path.join(sys.prefix, 'Lib/site-packages/pyidm'),['requirements.txt'])],
-    # package_data={
-    #     # can include only files inside the package:
-    #     "": ["*.txt"]
-    # },
-    # include_package_data=True,  # if plan to use MANIFEST.in
+    install_requires=requirements,
     entry_points={
-    	# our executable "exe file on windows for example"
+        # our executable: "exe file on windows for example"
         'console_scripts': [
             'pyidm = pyidm.pyIDM:main',
         ]},
